cde9c346e24c22ed375047b498da89a8c595308f
Russia Madden <dataengineer007@gmail.com>
2018-06-28 20:28:45 -0700

    notes on this and methods

--- todos_v3_objects/todos.js
+++ todos_v3_objects/todos.js
@@ -1,4 +1,6 @@
-// It should store the todos array on an object
+// Put everything so far on an object that represents a todoList
+//  change the stand alone functions to a method on the todoList object
+//  It should store the todos array on an object
 var todoList = {
     todos: ['item1', 'item2'],
     // It should have a displayTodos method
@@ -6,16 +8,16 @@ var todoList = {
         console.log(this.todos);
     },
     addTodos: function(newTodo) {
-      this.todos.push(newTodo);
+      todoList.todos.push(newTodo); // this works but if variable name changes must be updated everywhere
       todoList.displayTodos();
     },
     changeTodos: function(position, newTodo) {
         this.todos[position] = newTodo;
-        todoList.displayTodos();
+        this.displayTodos();
     },
     deleteTodos: function(position) {
         this.todos.splice([position], 1);
-        todoList.displayTodos();
+        this.displayTodos();
     }
 };
 
@@ -23,7 +25,7 @@ var todoList = {
 var russia = {
     name: 'Russia', // name property is an empty string?
     sayName: function () {
-        debugger;
+        // debugger;
         console.log(this); // prints the entire object
         console.log(name); // empty string/line? why no variable not defined error?
         console.log(this.name); // prints 'Russia' this refers to the Russia.name property value?
@@ -32,4 +34,5 @@ var russia = {
 };
 
 // name and sayName are object properties
-// sayName is a function on an object which is called a method
+// sayName is an anonymous function and is called by referencing the property name
+// a function on an object which is called a method
