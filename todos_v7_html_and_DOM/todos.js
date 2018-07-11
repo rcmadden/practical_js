8ebc3fbeae24956a5526afcf0016131189d0d56b
Russia Madden <dataengineer007@gmail.com>
2018-07-10 20:23:54 -0700

    add display and toggle buttons and event listeners

---
+++ todos_v7_html_and_DOM/todos.js
@@ -0,0 +1,81 @@
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        var totalTodos = this.todos.length;
+        var todo = this.todos;
+        if (totalTodos === 0) {
+            console.log('Your todo list is empty, yo!')
+        } else {
+            console.log('My Todos:')
+            for (var i = 0; i < totalTodos; i++) {
+                if (todo[i].completed === true) {
+                    console.log('(x) ', todo[i].todoText);
+                } else {
+                        console.log('( ) ', todo[i].todoText);
+                }
+            }
+
+        }
+
+
+    },
+    addTodos: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos;
+        todo[position].completed = !todo[position].completed;
+        this.displayTodos();
+    },
+
+    toggleAll: function() {
+        var todo = this.todos;
+        var totalTodos = todo.length;
+        var completedTodos = 0;
+        // get count of completed todos
+        for (var i = 0; i < totalTodos; i++) {
+            if (todo[i].completed === true) {
+                completedTodos++;
+            }
+        }
+        // Case1 if all are true then make all false
+        if (completedTodos === totalTodos) {
+                for (var i = 0; i < totalTodos; i++) {
+                    todo[i].completed = false;
+                }
+        // Case2 if all are false make all true
+        } else {
+            for (var i = 0; i < totalTodos; i++) {
+                todo[i].completed = true;
+                }
+            }
+            this.displayTodos();
+        }
+
+
+};
+
+
+// There should be a "Display Todos" button and a "Toggle all" button in the app
+// 1. get access to the display todos button
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+// console.log(displayTodosButton);
+// 2. run displayTodos method when someone clicks display todos button
+displayTodosButton.addEventListener('click', function() {
+   todoList.displayTodos();
+});
+
+
+// clicking "Display Todos" should run todoList.displayTodos
+
+
+// clicking "Toggle all" should run todoList.toggleAll
+var toggleAllButton = document.getElementById('toggleAllButton');
+toggleAllButton.addEventListener('click', function() {
+    todoList.toggleAll();
+});
